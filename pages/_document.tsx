17b3cf04bb7506b09d273fd066a41b4253b625ab
MrProscrite <lucas-ramos8@hotmail.com>
2022-10-31 13:00:35 -0300

    Try 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import type { DocumentProps } from 'next/document'
 import i18nextConfig from '../next-i18next.config'
+import nextI18nextConfig from '../next-i18next.config';
 
 type Props = DocumentProps & {
 }
 
 class MyDocument extends Document<Props> {
   render() {
-    const currentLocale =
-      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
-
-      console.log(i18nextConfig)
+    const currentLocale = this.props.__NEXT_DATA__.locale ?? process.env.DEFAULT_LANGUAGE;
     return (
       <Html lang={currentLocale}>
         <Head>
