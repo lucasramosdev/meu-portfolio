7b80f36b7340251fba54cbd1d6a794abce601209
MrProscrite <lucas-ramos8@hotmail.com>
2022-11-02 11:00:12 -0300

    Modals responsivos
    ajustando responsividade dos modais

--- components/projects/Modal.tsx
+++ components/projects/Modal.tsx
@@ -1,12 +1,14 @@
 import { Modal, Box, Typography, Grid } from '@mui/material';
 import { Trans } from 'next-i18next';
 import Image from 'next/image';
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { IconContext } from 'react-icons';
 import Carousel from 'react-multi-carousel';
 import { useMain } from '../../context';
 
 const MediaModal = ({props}: any) => {
+	const {width, height, percentage} = useWindowSize();
+	console.log()
 	const {state} = useMain()
   const theme = state.theme.palette;
 	
@@ -15,13 +17,9 @@ const MediaModal = ({props}: any) => {
 
 	const style = {
 		position: 'absolute',
-		top: {
-			xs: '0%',
-			sm: '2%',
-			lg: '5%'
-		},
+		top: '50%',
 		left: '50%',
-		transform: 'translate(-50%, 0%)',
+		transform: 'translate(-50%, -50%)',
 		width: '80%',
 		bgcolor: theme.background.default,
 		border: '2px solid #000',
@@ -58,7 +56,8 @@ const MediaModal = ({props}: any) => {
 				 	autoPlay={card?.images.length > 1 ? true : false}
 					autoPlaySpeed={2000}
           infinite={card?.images.length > 1 ? true : false}
-          containerClass="first-carousel-container container"
+          containerClass="first-carousel-container container modal"
+					itemClass='modal-image'
 					responsive={responsive}
 					deviceType='default'
 					showDots={card?.images.length > 1 ? true : false}
@@ -67,20 +66,20 @@ const MediaModal = ({props}: any) => {
 						<div style={{display: 'flex', justifyContent: 'center'}} key={idx}>
 							<Image
 								src={`/static/screenshots/${image}`}
-								width='500px'
-								height='500px'
+								width={percentage  * height + ' px'}
+								height={percentage  * height + ' px'}
 							/>
 						</div>
 					))}
 				</Carousel>
 				<Grid container margin='2rem auto 0 auto'>
 					<Grid item xs={12} lg={6} margin='1rem auto'>
-						<Typography variant='h4' textAlign='center' fontWeight='bolder'>
+						<Typography variant='h5' textAlign='center' fontWeight='bolder'>
 							Features
 						</Typography>
 						<ul style={{padding: '0 1rem'}}>
 						{card?.features.map((feature: string, idx: string) => (
-								<li style={{margin: '0.5rem auto', fontSize: '1.2rem'}} key={idx}>
+								<li style={{margin: '0.5rem auto', fontSize: '1rem'}} key={idx}>
 									<Trans i18nKey={feature} />
 								</li>
 							))}
@@ -90,9 +89,9 @@ const MediaModal = ({props}: any) => {
 						<hr style={{height: '100%', width: '10px', backgroundColor: theme.background.paper}}/>
 					</Grid>
 					<Grid item xs={12} lg={5} margin='0.5rem auto 0 auto'>
-						<Typography variant='h4' textAlign='center' fontWeight='bolder'>
+						<Typography variant='h5' textAlign='center' fontWeight='bolder'>
 							<Trans i18nKey='tecnologias'/>
-							<IconContext.Provider value={{color: 'primary', size: "3rem"}}>
+							<IconContext.Provider value={{color: 'primary', size: "2rem"}}>
 								<Grid container display='flex' justifyContent='center'>
 									{card?.tecnologies.map((icon: any, idx: string) => renderIcon(icon, idx))}
 								</Grid>
@@ -103,6 +102,41 @@ const MediaModal = ({props}: any) => {
 			</Box>
 		</Modal>
 	)
+}
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+		percentage: 0
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+				if(window.innerWidth >= 1200){
+					setWindowSize({
+						width: window.innerWidth,
+						height: window.innerHeight,
+						percentage: 0.5
+					})
+				}
+				else {
+					setWindowSize({
+						width: window.innerWidth,
+						height: window.innerHeight,
+						percentage: 0.3
+					})
+				}
+      }
+    
+			window.addEventListener("resize", handleResize);
+      handleResize();
+    
+			return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+  return windowSize;
 }
 
 export default MediaModal
